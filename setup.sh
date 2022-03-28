d4bebef62b67d96ba4bd89a8427ce55ec1def422
mizukenwater <mizuken.water@gmail.com>
2022-03-28 19:09:16 +0900

    fix: Fix setup command

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose run --rm app rails new . -d mysql --api -M -C -S -J --skip-spring --skip-turbolinks --skip-webpack-install -T --minimal
+docker compose build && docker compose run --rm app rails new . -d mysql --api -M -C -S -J --skip-spring --skip-turbolinks --skip-webpack-install -T --minimal
